173717c41efe12016ff434fc725c1bfb59fe6631
Sam Perry <snperry89@gmail.com>
2021-06-03 16:17:07 -0400

    fixed connection

---
+++ Rockwood.Data/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rockwood.Data
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        //[Required]
+        //public Guid OwnerId { get; set; }
+
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+    }
+}
