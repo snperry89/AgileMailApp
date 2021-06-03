0b7bfba266393273b6bee5d640f9d9f23cffc2b9
Sam Perry <snperry89@gmail.com>
2021-06-03 17:21:44 -0400

    finished Post and Get methods

--- Rockwood.Models/UserListItem.cs
+++ Rockwood.Models/UserListItem.cs
@@ -1,27 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Rockwood.Data
+namespace Rockwood.Models
 {
-    public class User
+    public class UserListItem
     {
-        [Key]
         public int UserId { get; set; }
 
-        [Required]
-        public Guid OwnerId { get; set; }
-
-        [Required]
         public string EmailAddress { get; set; }
 
-        [Required]
         public string FirstName { get; set; }
 
-        [Required]
         public string LastName { get; set; }
     }
 }
--- Rockwood.Services/UserService.cs
+++ Rockwood.Services/UserService.cs
@@ -1,29 +1,56 @@
-﻿using Rockwood.Models;
+﻿using Rockwood.Data;
+using Rockwood.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rockwood.Services
 {
     public class UserService
     {
+        private readonly Guid _userId;
+        public UserService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateUser(UserCreate model)
+        {
+            var entity = new User()
+            {
+                EmailAddress = model.EmailAddress,
+                FirstName = model.FirstName,
+                LastName = model.LastName
+            };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Users.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<UserListItem> GetUsers()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Users
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                        new UserListItem
+                        {
+                            UserId = e.UserId,
+                            EmailAddress = e.EmailAddress,
+                            FirstName = e.FirstName,
+                            LastName = e.LastName
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
 
 
-
-        //public bool CreateUser(UserCreate model)
-        //{
-        //    var entity = new UserService()
-        //    {
-        //        EmailAddress = model.EmailAddress,
-        //        FirstName = model.FirstName,
-        //        LastName = model.LastName
-        //    };
-        //    using (var ctx = new ApplicationDbContext())
-        //    {
-        //        ctx.Users.Add
-        //    }
-        //}
     }
 }
